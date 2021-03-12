591a9127add7142b54f620eeee21aed2d4d8146b
rodrigodojo <rodrigodoj@gmail.com>
2021-03-12 16:57:46 -0300

    Criando um layout basico inicial e a classe JsonTask para obter os resultados dos jogos.

--- app/src/main/java/com/dojo/nowsportsreviews/MainActivity.java
+++ app/src/main/java/com/dojo/nowsportsreviews/MainActivity.java
@@ -3,8 +3,13 @@ package com.dojo.nowsportsreviews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    public ImageButton btNews;
+    public ImageButton btHome;
+    public String minhaUrl = "test_20de5f25bb8d40e6b7f110d393e57c";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
